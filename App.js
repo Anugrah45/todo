3e05058692bb3ad0f9b2add10667bc095595e007
Anugrah45 <120392592+Anugrah45@users.noreply.github.com>
2022-12-12 20:16:13 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+//import Reg from './Registration/Reg'
+
+//import Calculator from './Calculator/Calculator'
+//import Product from './Product/Product'
+//import Header from './Restaurant/Header'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ToDo from './Todo/ToDo';
+//import Home from './Restaurant/Home';
+function  App() {
+
+  return (
+    <div>
+          <ToDo/>
+          
+    </div>
+
+  )
+}
+
+export default App
+
